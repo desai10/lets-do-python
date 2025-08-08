a4efa849db209ed9c8bdbc4953dd27a2931c06e5
Sriram Desai <isriramdesai@gmail.com>
2025-08-08 22:52:38 +0800

    python data type
    Change-Id: I3b02ca6dcdf71754cf533f32470d98be510f8247

---
+++ data_types.py
@@ -0,0 +1,12 @@
+name = "sriram"
+age = 28
+
+# print(name + " is " + str(age) + " years old")
+print(f"{name} is {age} years old")
+
+# name_type = type(name)
+# name_type_str = str(name_type)
+
+# print(type(name_type))
+
+# print("type of name is " + name_type_str)
