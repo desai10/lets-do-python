1a0d2d96fbdd771ca15b7c4d97e8030aeeb5d851
Sriram Desai <isriramdesai@gmail.com>
2025-08-08 23:35:41 +0800

    python arr and dict
    Change-Id: I01062a9bfb027b7b56059c0b5186eeaea220d173

---
+++ more_data_types.py
@@ -0,0 +1,35 @@
+# arr = []
+# arr.append(123)
+# arr.append(234)
+# arr.append("sriram")
+
+# print(arr)
+# print(f"type of arr[1] is {type(arr[1])}")
+# print(f"type of arr[2] is {type(arr[2])}")
+
+# marks = {}
+# print(type(marks))
+
+# marks["Mathematics"] = 94
+# marks["Science"] = 92
+# marks["SS"] = 80.5
+# marks[123] = 100
+
+# print(marks)
+# print(f"type of math marks is {type(marks["Mathematics"])}")
+# print(f"type of SS marks is {type(marks["SS"])}")
+
+# list_to_sum = {}
+
+# arr = [1, 2, 3]
+# list_to_sum[arr] = 6
+
+# arr.append(23)
+
+# print(list_to_sum)
+
+int_to_arr = {}
+
+int_to_arr[1] = [1, 2, 3]
+
+print(int_to_arr)
